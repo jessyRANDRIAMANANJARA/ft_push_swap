8140f44af38ca26f59c0d609f48337bfcab78e52
Heriniaina Jessy Randriamananjara <hrandri2@c1r13s3.42antananarivo.mg>
2026-04-11 01:08:38 +0300

    add-repo

---
+++ Makefile
@@ -0,0 +1,46 @@
+NAME = push_swap
+CC = cc
+CFLAGS = -Wall -Werror -Wextra
+
+SRCS = main.c error_free.c push_command.c simple_sort.c \
+		push_swap_init.c reverse_rotate_command.c rotate_command.c \
+		stack_init.c stack_utils.c swap_command.c tiny_sort.c disorder.c \
+		medium_sort.c complex_sort.c bench.c operation.c rotate_utils.c \
+		gest_flag.c medium_sort_array.c medium_target.c simple_find_cost.c \
+		medium_move.c simple_move.c
+OBJS = $(SRCS:.c=.o)
+
+HEADERS = push_swap.h
+
+LIBFTPRINTF = ft_printf/libftprintf.a
+LIBFTFPRINTF = ft_fprintf/libftfprintf.a
+LIBFT = libft/libft.a
+
+all : $(NAME)
+
+$(LIBFTPRINTF):
+	make -C ft_printf
+$(LIBFTFPRINTF):
+	make -C ft_fprintf
+$(LIBFT):
+	make -C libft
+
+$(NAME) : $(OBJS) $(LIBFTPRINTF) $(LIBFTFPRINTF) $(LIBFT)
+	$(CC) $(CFLAGS) $(OBJS) -L./ft_printf -lftprintf -L./ft_fprintf -lftfprintf -L./libft -lft -o $(NAME)
+
+clean :
+	rm -f $(OBJS)
+	rm -f $(LIBFTPRINTF)
+	rm -f $(LIBFTFPRINTF)
+	rm -f $(LIBFT)
+
+
+fclean : clean
+	make -C ft_printf fclean
+	make -C ft_fprintf fclean
+	make -C libft fclean
+	rm -f $(NAME)
+
+re : fclean all
+
+.PHONY : all clean fclean re
---
+++ gest_flag.c
@@ -0,0 +1,107 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   gest_flag.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hrandri2 <hrandri2@student.42antananari    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/04/07 20:23:01 by hrandri2          #+#    #+#             */
+/*   Updated: 2026/04/07 20:50:40 by hrandri2         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ft_push_swap.h"
+
+void	adaptive_flag(int disorder_percent, t_stack_node **a,
+				t_stack_node **b, t_count *count)
+{
+	if (disorder_percent < 20)
+		push_swap(a, b, count);
+	else if (disorder_percent >= 20 && disorder_percent < 50)
+		medium_sort(a, b, count);
+	else
+		radix_sort(a, b, count);
+}
+
+void	apply_sort_flag(char *arg, t_stack_node **a, t_stack_node **b,
+			t_count *count)
+{
+	if (!arg || ft_strcmp(arg, "--adaptive") == 0
+		|| ft_strcmp(arg, "--bench") == 0)
+		adaptive_flag(count->disorder_percent, a, b, count);
+	else if (ft_strcmp(arg, "--simple") == 0)
+		push_swap(a, b, count);
+	else if (ft_strcmp(arg, "--medium") == 0)
+		medium_sort(a, b, count);
+	else if (ft_strcmp(arg, "--complex") == 0)
+		radix_sort(a, b, count);
+	else
+		(write(2, "Error\n", 6), exit(1));
+}
+
+char	*get_sort_flag(int argc, char **argv)
+{
+	int	i;
+
+	i = 1;
+	while (i < argc)
+	{
+		if (ft_strncmp(argv[i], "--", 2) == 0
+			&& ft_strcmp(argv[i], "--bench") != 0)
+			return (argv[i]);
+		i++;
+	}
+	return (NULL);
+}
+
+char	*repeat_flag(char **arg)
+{
+	int		i;
+	int		j;
+	int		flags;
+	char	*f[4];
+
+	f[0] = "--simple";
+	f[1] = "--medium";
+	f[2] = "--complex";
+	f[3] = "--adaptive";
+	flags = 0;
+	i = -1;
+	while (arg[++i])
+	{
+		j = -1;
+		while (++j < 4)
+			if (ft_strcmp(f[j], arg[i]) == 0 && ++flags > 1)
+				return (arg[i]);
+		j = i;
+		while (arg[++j])
+			if (ft_strcmp(arg[i], arg[j]) == 0)
+				return (arg[i]);
+	}
+	return (NULL);
+}
+
+bool	check_flags(int argc, char **argv, t_args *args)
+{
+	int	start;
+
+	if (repeat_flag(argv))
+		return (ft_printf("Error\n"), true);
+	args->flag = get_sort_flag(argc, argv);
+	args->bench = has_bench_flag(argc, argv);
+	args->values = NULL;
+	args->free_values = false;
+	if (argc < 2)
+		return (true);
+	start = 1;
+	while (start < argc && ft_strncmp(argv[start], "--", 2) == 0)
+		start++;
+	if (start >= argc)
+		return (true);
+	args->free_values = (start == argc - 1);
+	if (args->free_values)
+		args->values = ft_split(argv[start], ' ');
+	else
+		args->values = argv + start;
+	return (false);
+}
